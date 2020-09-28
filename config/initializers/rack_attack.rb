a240a9a60a883683d2fea8a5746c23803b8cadb7
gianniresende <gianniresende@gmail.com>
2020-09-28 09:20:20 -0300

    feat: add configs rack-cors, rack-attack

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,48 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+class Rack::Attack
+ 
+ Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+ 
+ # Allow all local traffic
+ 
+ safelist('allow-localhost') do |req|
+   '127.0.0.1' == req.ip || '::1' == req.ip
+ end
+ 
+ # Allow an IP address to make 10 requests every 10 seconds
+ 
+ throttle('req/ip', limit: 5, period: 5) do |req|
+   req.ip
+ end
+ 
+ # Throttle login attempts by email address
+ #throttle("logins/email", limit: 5, period: 20.seconds) do |req|
+ #  if req.path == '/users/sign_in' && req.post?
+ #    req.params['email'].presence
+ #  end
+ #end
+ 
+end
